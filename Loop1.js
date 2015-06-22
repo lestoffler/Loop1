07c4bc56818068561624caf9255aa8806543c7bf
Student <lestoffler@gmail.com>
2015-06-22 12:22:55 -0600

    first commit

---
+++ Loop1.js
@@ -0,0 +1,32 @@
+for (var i = 0; i <= 500; i+= 100) {
+	console.log(i);
+}
+
+for (var i = 1; i <= 64; i *= 2) {
+	console.log(i);
+}
+
+for (var i = 1; i <= 3; i++) {
+	console.log(i, i, i);
+	for (var i = 2; i <= 3; i++) {
+		console.log (i, i, i);
+	}
+}
+
+for (var i = 0; i <= 10; i+= 2) {
+	console.log(i);
+}
+
+for (var i = 3; i <= 15; i += 3) {
+	console.log(i);
+}
+
+for (var i = 9; i >= 0; i--) {
+	console.log(i);
+}	
+
+for (var i = 0; i <= 3; i++) {	
+	for (var f = 0; f <= 3; f++) {
+		console.log(f);
+	}
+}
